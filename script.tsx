258a7889a672a9ae43ec7058f5bc942fe2dc9564
star development labs <87822123+star08-web@users.noreply.github.com>
2022-07-10 19:04:16 +0200

    Add files via upload

---
+++ script.tsx
@@ -0,0 +1,3 @@
+console.log('Ciao Curiosone =) Vedo Che Ti interessa guardare il sito "sotto la scocca"... ');
+console.log('se ti interessa il codice sorgente puoi trovarlo  Qui --> https://github.com/star08-web/ilmondodimattia/');
+console.log('Divertiti ad Hackerarlo =)');
